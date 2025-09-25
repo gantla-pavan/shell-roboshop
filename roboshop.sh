05aa235d5aab3ed7535a2d3511fc4ccd601091cd
gantlapavan <gantlapavan1705@gmail.com>
2025-09-25 17:31:57 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ for instances in $@
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a25a19c0d44231b7 --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"  --query 'Instances[0].InstanceId' --output text)
 
-    if[ $instances != "frontend" ];then
+    if [ $instances != "frontend" ];then
         IP=$(aws ec2 describe-instances --instance-ids i-0701657d289653a75 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
     else
