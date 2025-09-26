65749fe3c2c32f1ed17357b56a6fe53e5a7a3bd4
gantlapavan <gantlapavan1705@gmail.com>
2025-09-26 20:24:47 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -4,13 +4,13 @@ SG_ID="sg-0f08420df1833f423"
 
 for instance in $@
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0f08420df1833f423 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstancesId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID--instance-type t3.micro --security-group-ids $SGI_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstancesId' --output text)
 
     if [ $instance != "frontend" ];then
-        IP=$(aws ec2 describe-instances --instance-ids ami-09c813fb71547fc4f --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
     else
-         IP=$(aws ec2 describe-instances --instance-ids ami-09c813fb71547fc4f --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE-ID--query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
     echo "$instance: $IP"
 done
